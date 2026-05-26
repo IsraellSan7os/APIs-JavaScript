42440de3a0ea513bc9017789042b3ad8c5326ac3
Israel Santos <ysraellsanosss@gmail.com>
2026-05-25 22:34:29 -0300

    Utilizando async e await

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-fetch("http://localhost:3333/products")
-.then((response) => response.json())
-.then((date) => console.log(date))
+// Utilizando o fetch com then.
+// fetch("http://localhost:3333/products")
+// .then((response) => response.json())
+// .then((date) => console.log(date))
+
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products");
+  const date = await response.json();
+  console.log(date);
+}
+
+fetchProducts()
