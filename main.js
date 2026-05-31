fca44f2210dfbc921660e4f2665c5a6a7e511649
Israel Santos <ysraellsanosss@gmail.com>
2026-05-31 00:56:45 -0300

    Estudos fetch com post

--- main.js
+++ main.js
@@ -5,8 +5,49 @@
 
 async function fetchProducts() {
   const response = await fetch("http://localhost:3333/products");
-  const date = await response.json();
-  console.log(date);
+  const data = await response.json();
+  console.log(data);
 }
 
-fetchProducts()
+//fetchProducts()
+
+async function fetchProductById(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+// fetchProductById(1);
+// fetchProductById(2);
+// fetchProductById(3);
+
+async function fetchUser(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+// fetchUser(1);
+// fetchUser(2);
+// fetchUser(3);
+
+const ProductName = document.getElementById("name");
+const ProductPrice = document.getElementById("price");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  await fetch("http://localhost:3333/products", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime().toString(),
+      name: ProductName.value,
+      price: ProductPrice.value,
+    }),
+  });
+
+  await fetchProducts();
+});
